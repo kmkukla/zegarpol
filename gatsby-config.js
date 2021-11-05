160565e83c44207e7201652f41120f6825124d01
Konrad Kukla <kondziocard@gmail.com>
2021-11-05 14:02:47 +0100

    add gatsby-plugin-scroll-reveal for animations

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,44 +1,50 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: 'https://www.yourdomain.tld',
-    title: 'zegarpol',
+    siteUrl: "https://www.yourdomain.tld",
+    title: "zegarpol",
   },
   plugins: [
-    'gatsby-plugin-styled-components',
-    'gatsby-plugin-image',
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: "gatsby-plugin-google-analytics",
       options: {
-        trackingId: 'G-HE2J3WWVMK',
+        trackingId: "G-HE2J3WWVMK",
       },
     },
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'images',
-        path: './src/images/',
+        name: "images",
+        path: "./src/images/",
       },
-      __key: 'images',
+      __key: "images",
     },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'GatsbyJS',
-        short_name: 'GatsbyJS',
-        start_url: '/',
-        background_color: '#6b37bf',
-        theme_color: '#6b37bf',
+        name: "GatsbyJS",
+        short_name: "GatsbyJS",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: 'standalone',
-        icon: 'src/images/zegarpol-logo.png', // This path is relative to the root of the site.
+        display: "standalone",
+        icon: "src/images/zegarpol-logo.png", // This path is relative to the root of the site.
         // An optional attribute which provides support for CORS check.
         // If you do not provide a crossOrigin option, it will skip CORS for manifest.
         // Any invalid keyword or empty string defaults to `anonymous`
         crossOrigin: `use-credentials`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-scroll-reveal`,
+      options: {
+        once: false, // Defines if animation needs to be launched once
       },
     },
   ],
