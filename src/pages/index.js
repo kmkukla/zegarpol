c2fd5778435c15defe67a6463427b94f5abd1d15
Konrad Kukla <kondziocard@gmail.com>
2021-11-10 23:10:41 +0100

    add react-tsparticles

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,41 @@ import OfferSection from "../components/sections/Offer/OfferSection";
 import OpeningHoursSection from "../components/sections/OpeningHours/OpeningHoursSection";
 import LocationSection from "../components/sections/Location/LocationSection";
 import ContactSection from "../components/sections/Contact/ContactSection";
+import Particles from "react-tsparticles";
+import Footer from "../components/Footer/Footer";
 
 const IndexPage = () => {
   return (
     <>
+      <Particles
+        id="tsparticles"
+        params={{
+          particles: {
+            color: "#C0DBA7",
+            number: { value: 10, density: { enable: true, value_area: 800 } },
+            shape: {
+              type: ["circle"],
+            },
+            opacity: {
+              value: 0.5,
+              random: true,
+              anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
+            },
+            size: { value: 10, random: false },
+            line_linked: { enable: false },
+            move: {
+              enable: true,
+              speed: 1.5,
+              direction: "none",
+              random: false,
+              straight: false,
+              bounce: true,
+              attract: { enable: true, rotateX: 100, rotateY: 400 },
+            },
+          },
+          retina_detect: true,
+        }}
+      />
       <GlobalStyle />
       <Navigation />
       <AboutSection />
@@ -17,6 +48,7 @@ const IndexPage = () => {
       <OpeningHoursSection />
       <LocationSection />
       <ContactSection />
+      <Footer />
     </>
   );
 };
